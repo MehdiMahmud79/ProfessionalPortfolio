0d818928e1fefb0318ef39efbbca35c9ed9fec9e
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-08-13 12:06:24 +0100

    Need to be fixed- project languages

---
+++ README.md
@@ -0,0 +1,52 @@
+# Advanced CSS: Portfolio
+## I'm Mehdi and I'm a <span>web developer</span>
+### PhD finally over!
+#### Now I can fully concentrate on Coding Boot Camp for 6 months and become a full web developer
+```
+Experience in software, IT and research. I completed a PhD in Applied Mathematics at the University of Birmingham. I worked as a researcher at the same university and now participating in a 6 month coding bootcamp to become a web developer.
+```
+
+
+## Mock-Up
+
+The following animation shows the web application's appearance and functionality:
+
+![portfolio demo](./Assets/images/Portfolio.gif)
+
+
+
+## Websites
+* Github: https://github.com/MehdiMahmud79/Portfolio
+* Deployed: https://mehdimahmud79.github.io/Portfolio/
+
+- - -
+© 2021 .
+
+
+
+<!-- ## Acceptance Criteria 
+
+Here are the critical requirements necessary to develop a portfolio that satisfies a typical hiring manager’s needs:
+
+```
+#GIVEN I need to sample a potential employee's previous work
+WHEN I load their portfolio
+
+#THEN I am presented with the developer's name, a recent photo or avatar, and links to sections about them, their work, and how to contact them
+
+#WHEN I click one of the links in the navigation
+THEN the UI scrolls to the corresponding section
+
+#WHEN I click on the link to the section about their work
+THEN the UI scrolls to a section with titled images of the developer's applications
+
+WHEN I am presented with the developer's first application
+THEN that application's image should be larger in size than the others
+
+WHEN I click on the images of the applications
+THEN I am taken to that deployed application
+
+WHEN I resize the page or view the site on various screens and devices
+THEN I am presented with a responsive layout that adapts to my viewport
+```
+-->
---
+++ assets/js/script.js
@@ -0,0 +1,253 @@
+var projects = [];
+var projects_lang = [];
+var projObj={};
+const log=console.log;
+getApi();
+
+function getApi() {
+  // replace `octocat` with anyone else's GitHub username
+  username = "mehdimahmud79";
+  var requestUrl = `https://api.github.com/users/${username}/repos`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // log(data)
+      for (var i = 0; i < data.length; i++) {
+        // var proj_name = `${data[i].full_name.split("/")[1]}`;
+        var proj_name = data[i].name;
+        projectUrl = `https://mehdimahmud79.github.io/${proj_name}/`;
+
+         projObj= {
+          projectName: proj_name,
+          gitHub_Url: `${data[i].owner.html_url}/${proj_name}`,
+          description: data[i].description,
+          project_Url: projectUrl,
+          project_img: `https://github.com/MehdiMahmud79/${proj_name}/blob/main/assets/screen.gif?raw=true`,
+          lang_url : data[i].languages_url,
+          lang:{}
+        };
+        // log("project_img", projObj);
+        projects.push(projObj)
+
+ 
+
+
+        // fetch(lang_url)
+        //   .then(function (response2) {
+        //     return response2.json();
+        //   })
+        //   .then(function (data2) {
+        //     console.log(data2);
+        //     let sum1 = 0;
+        //     let lanObj = { url: proj_name };
+        //     if (data2.CSS) sum1 += data2.CSS;
+        //     if (data2.HTML) sum1 += data2.HTML;
+        //     if (data2.JavaScript) sum1 += data2.JavaScript;
+
+        //     if (data2.CSS)
+        //       lanObj.CSS = `${Math.round((data2.CSS / sum1) * 100)} %`;
+        //     if (data2.HTML)
+        //       lanObj.HTML = `${Math.round((data2.HTML / sum1) * 100)} %`;
+        //     if (data2.JavaScript)
+        //       lanObj.JavaScript = `${Math.round(
+        //         (data2.JavaScript / sum1) * 100
+        //       )} %`;
+        //     projects_lang.push(lanObj);
+
+        //     // if(data2.CSS)console.log(`CSS: ${projObj.CSS}`);
+        //     // if(data2.HTML)console.log(`HTML: ${projObj.HTML}`);
+        //     // if(data2.JavaScript)console.log(`JavaScript: ${projObj.JavaScript}`);
+        //   });
+      }
+      
+      // for (var i = 0; i < data.length; i++) {
+      // //   var listItem = document.createElement('li');
+      // //   listItem.textContent = data[i].html_url;
+      // //   repoList.appendChild(listItem);
+      // console.log(data[i].html_url)
+      // }
+    });
+}
+// function myFunction() {
+//   var dots = document.getElementById("dots");
+//   var moreText = document.getElementById("more");
+//   var btnText = document.getElementById("myBtn");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
+setTimeout(function () {
+
+  projects.forEach(project=>{
+
+  const lang_url =project.lang_url;
+  async function myFetch() {
+    let response = await fetch(lang_url);
+    if (!response.ok) {
+      throw new Error(`HTTP error for the language request! status: ${response.status}`);
+    }
+    return await response.json();
+  
+  }
+  myFetch().then((data2) => {
+    // console.log(data2);
+    let sum1 = 0;
+    let lanObj = {};
+    if (data2.CSS) sum1 += data2.CSS;
+    if (data2.HTML) sum1 += data2.HTML;
+    if (data2.JavaScript) sum1 += data2.JavaScript;
+
+    if (data2.CSS)
+      lanObj.CSS = `${Math.round((data2.CSS / sum1) * 100)} %`;
+    if (data2.HTML)
+      lanObj.HTML = `${Math.round((data2.HTML / sum1) * 100)} %`;
+    if (data2.JavaScript)
+      lanObj.JavaScript = `${Math.round(
+        (data2.JavaScript / sum1) * 100
+      )} %`;
+
+      project.lang=  lanObj;
+      // projects.push(projObj);
+// log(lanObj)
+    // projects_lang.push(lanObj);
+  }).catch(e => console.log(e));
+
+  // projObj= {
+  //   projectName: proj_name,
+  //   gitHub_Url: `${data[i].owner.html_url}/${proj_name}`,
+  //   description: data[i].description,
+  //   project_Url: projectUrl,
+  //   project_img: `https://github.com/MehdiMahmud79/${proj_name}/blob/main/assets/screen.gif?raw=true`,
+  //   lang_url : data[i].languages_url,
+  //   lang:{}
+  // };
+
+  var mycard = `
+  <div class="col my-3 gradient-custom">
+  <div class="card">
+    <img src="${project.project_img}" class="card-img-top " alt="..."/>
+    <div class="  bg-dark d-flex justify-content-around">
+      <a type="button" href="${project.gitHub_Url}" class="text-info text-decoration-none"><i class="fab fa-github-alt"></i> Github</a>
+      <a type="button" href="${project.description}" class="text-info text-decoration-none"><i class="fab fa-internet-explorer"></i> Live</a>
+    </div>
+
+    <div class="card-body">
+      <h5 class="card-title">${project.projectName}</h5>
+      <p class="card-text">${project.project_Url}</p>
+    </div>
+    <div class="progress m-2">
+    <div class="progress-bar bg-success text-dark" role="progressbar" style="width: 60%" >60%</div>
+    <div class="progress-bar bg-warning" role="progressbar" style="width: 50%" >50%</div>
+    <div class="progress-bar bg-danger" role="progressbar" style="width: 30%">30%</div>
+    </div>
+  </div>
+
+  
+</div>
+
+`;
+  $(".project-fetched").append(mycard);
+  })
+        
+
+  
+ 
+  // console.log("projects lang", projects_lang);
+  // console.log("projects", projects);
+  // console.log("projects length", projects.length, projects_lang.length);
+  // projects.forEach((item,index)=>{
+  //     console.log(item,index);
+  //     // item.pushed=projects_lang[index];
+
+  // })
+
+  //   var  mycard=`
+  //   <div class="flex flex-grow items-stretch m-4 p-1 transform scale-110 filter active">
+  //   <figure class="code-card ">
+  //   <h2 class="card-header">${projects[0].projectName}</h2>
+  //   <div class="catagories ">
+  //   <p class="desc">${projects[0].description.slice(0,60)}<span id="dots">...</span><span id="more">${projects[0].description.slice(60,projects[0].description.length)}</span></p>
+
+  //   <button class="readmore" id="myBtn" onclick="myFunction()">Read more</button>
+
+  //   <div class="image">
+  //           <p>
+  //           <a href="${projects[0].projectUrl}" target="_blank">
+
+  //           <img src="https://github.com/MehdiMahmud79/${projects[0].projectName}/blob/main/assets/screen.gif?raw=true" alt="horision website"></a>
+
+  //           </p>
+  //       </div>
+  //       <div class="flex flex-col justify-around">
+  //        <a  href="${projects[0].project_Url}"><i class="text-red-900 fab fa-internet-explorer"> </i>Deployed</a>
+  //        <a href="${projects[0].gitHub_Url}"><i class="text-red-900 fab fa-github "></i>Git Hub</a>
+  //       </div>
+
+  //   </div>
+  //   <div class="progress">
+  //   <div class="progress-bar bg-warning text-dark" role="progressbar" style="width: 60%" >60%</div>
+  //   <div class="progress-bar bg-success" role="progressbar" style="width: 50%" >50%</div>
+  //   <div class="progress-bar bg-danger" role="progressbar" style="width: 30%">30%</div>
+  // </div>
+  // <p class="fs-6"> Javascript: 60%  CSS: 20% HTML: 10%  </p>
+  // </figure>
+  // </div>
+
+
+
+
+}, 200); //wait 2 seconds
+log(projects)
+
+// function closemenue() {
+//   var mybox = document.getElementById("menulist");
+//   if (mybox.style.display == "none") {
+//     mybox.style.display = "inline-block";
+//   } else {
+//     mybox.style.display = "none";
+//   }
+// }
+
+$(document).ready(function () {
+  // typing text animation script
+
+  var typed = new Typed(".skillTitle span", {
+    strings: ["WebDeveloper.", "WebDeveloper."],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".Myskills", {
+    strings: [
+      "HTML",
+      "JavaScript",
+      "JQuery",
+      "NodeJs",
+      "Express", 
+      "Git",           
+      "CSS",
+      "BootStrap",
+      "Git",
+      "Fortran",
+      "Visual Basic",
+      "Microsoft office",
+      "Matlab",
+      "Adobe Premier",
+
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  });
+});
