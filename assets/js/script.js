f4df44c26019f1f48762fa1ee70572387503a837
Mehdi <84668631+MehdiMahmud79@users.noreply.github.com>
2021-10-05 23:59:15 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,7 +174,7 @@ function creatCarousel(favProject) {
 
     mycard = `
        <div class="carousel-item ${activeclass}">
-          <div class="text-center my-1"> <h5 class="display-5  text-red-900 ">${project.projectName.toUpperCase()}</h5></div>
+          <div class="text-center my-1"> <h5 class="display-5  text-red-900 ">${project.projectName}</h5></div>
           
           <div class="d-flex flex-column flex-md-row align-items-center mx-5">
                <div class="mx-2 text-center w-100 projectAll" >
